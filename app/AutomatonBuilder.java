ccdd75532495b70887d184d91dbb707e3eee2bc6
Equinox4 <equinox4@users.noreply.github.com>
2020-11-17 23:16:47 +0100

    renamed automataBuilder to automatonBuilder

---
+++ app/AutomatonBuilder.java
@@ -0,0 +1,28 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutomatonBuilder {
+    private ArrayList<Automaton> automatons;
+    private int currentState;
+    private static final String EPSILON = "epsilon";
+    private boolean concat = false;
+    private boolean altern = false;
+    private boolean closure = false;
+
+    public AutomatonBuilder() {
+        this.automatons = new ArrayList<>();
+        this.currentState = 0;
+    }
+
+    public List<Automaton> getAutomatons() {
+        return this.automatons;
+    }
+
+
+
+    public String toString() {
+        return this.automatons.toString();
+    }
+}
